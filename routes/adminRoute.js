1ece4a6c970951a84012e82bd199c5c06cf5eb65
Zeliang-Codetech <Zeliangcodetech@gmail.com>
2024-07-29 17:22:10 +0530

    Admin post handler update

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -4,40 +4,28 @@ const router = express.Router();
 var espData = {};
 
 
-// // POST endpoint to send messages
-// router.post("/", (req, res) => {
-//   const { id, message } = req.body;
-//     // check if the Id already exists
-    
-//   if (espData[id]) {
-//     // Update the existing ID with new readings
-//     espData[id].push(message);
-//       console.log(`Readings updated for ${id}: ${message}`);
-//         if (espData[id].length > 1) {
-//           espData[id].shift(); // Remove the oldest reading
-//         }
-//   } else {
-//     // Add a new ID with the message
-//     espData[id] = [message]; // Initialize with an array containing the first message
-//     console.log(`New ESP added: ${id}`);
-//   }
-//   // Handle POST request to /admin Route
-//   res.json({ status: "recieved by the admin route" });
-// });
-
-router.post('/', (req, res) => {
+// POST endpoint to send messages
+router.post("/", (req, res) => {
   const { id, message } = req.body;
+    // check if the Id already exists
+    
+  if (espData[id]) {
+    // Update the existing ID with new readings
+    espData[id].push(message);
+      console.log(`Readings updated for ${id}: ${message}`);
+        if (espData[id].length > 1) {
+          espData[id].shift(); // Remove the oldest reading
+        }
+  } else {
+    // Add a new ID with the message
+    espData[id] = [message]; // Initialize with an array containing the first message
+    console.log(`New ESP added: ${id}`);
+  }
+  // Handle POST request to /admin Route
+  res.json({ status: "recieved by the admin" });
+});
 
-    espData[id] = {
-      id: id,
-      message: message,
-  };
-    console.log(`Data received for ${id}: ${message}`);
 
-    // Respond to the POST request
-    res.json({ status: "received by the admin route" });
-
-})
 
 
 router.get("/", (req, res) => {
