6afd74231aa169244a90a8590f942153c5c5304d
Zeliang-Codetech <Zeliangcodetech@gmail.com>
2024-08-07 13:18:04 +0530

    update driver feature

--- routes/updateDriver.js
+++ routes/updateDriver.js
@@ -6,12 +6,15 @@ const { get } = require("http");
 var db;
 var drivers = [];
 
-router.get('/', (req, res) => {
-  res.status(200).json({message: "Update driver GET endpoint is working fine!"})
-})
+router.get("/", (req, res) => {
+  res
+    .status(200)
+    .json({ message: "Update driver GET endpoint is working fine!" });
+});
 
 router.post("/", (req, res) => {
   const { driverId, name, number } = req.body;
+
   // after getting the values connect to the mongoDB
   console.log(`${driverId},  ${name} ,  ${number}}`);
 
@@ -26,54 +29,29 @@ router.post("/", (req, res) => {
           drivers.push(driver);
           if (driverId == driver._id) {
             var nameAndNum = { $set: { driverName: name, Phone: number } };
-            db.collection('drivers').updateOne({}, nameAndNum, function (err, res) {
-              if (err) {
-                throw err; 
-                
-              } else {
-                console.log("1 document updated!");
-                db.close();
+            db.collection("drivers").updateOne(
+              {id: driverId},
+              nameAndNum,
+              function (err, res) {
+                if (err) {
+                  throw err;
+                } else {
+                  console.log("1 document updated!");
+                  db.close();
+                }
               }
-            })
-            // update the driver name and phone no here. 
+            );
+            // update the driver name and phone no here.
           }
         })
         .then(() => {
-          // for (let i = 0; i < bins[0].bins.length; i++) {
-          //   if (bins[0].bins[i].driver[0] == driverId) {
-          //     // let myDriverId = bins[0].bins[i].driver[0];
-          //     // let id = myDriverId.toString();
-          //     const filter = {};
-
-          //     // Define the data to update
-          //     const data = {
-          //       $set: {
-          //         "bins.$[bin].driver.$[driver].name": name,
-          //         "bins.$[bin].driver.$[driver].phone": number,
-          //       },
-          //     };
-
-          //     db.collection("bins").updateOne(
-          //       filter,
-          //       data,
-          //       (err, collection) => {
-          //         if (err) throw err;
-          //         console.log("Record updated successfully");
-          //         console.log(collection);
-          //       }
-          //     );
-          //   }
-          // }
-          
           console.log(drivers);
         });
     }
   });
-  res
-    .status(200)
-    .json({
-      messge: "Sent name and phone number to the /updateDriver endpoint",
-    });
+  res.status(200).json({
+    messge: "Name and phone number recived! ",
+  });
 });
 
 module.exports = router;
