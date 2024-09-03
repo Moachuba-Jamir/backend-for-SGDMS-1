0849708e6167cc7019c60c09f69c2b3ce70e36a7
Zeliang-Codetech <Zeliangcodetech@gmail.com>
2024-09-03 19:07:15 +0530

    bug fixes1

--- db.js
+++ db.js
@@ -1,25 +1,52 @@
-require("dotenv").config();
+// require("dotenv").config();
+// const { MongoClient } = require("mongodb");
+// var dbConnection;
+
+// module.exports = {
+//   // connect to the mongodb database
+//   connectToDb: (cb) => {
+//     MongoClient.connect(process.env.URI)
+//       .then((client) => {
+//         dbConnection = client.db("sgdms");
+//         console.log("Successfully connected to mongo db server");
+//         // function to fire after database connection
+//         return cb();
+//       })
+//       .catch((err) => {
+//         console.warn(`Error connecting to the database: ${err}`);
+//       });
+//   },
+
+//   // fetch and retrieve data from the mongo db
+//   getDB: () => {
+//     // return the database connection
+//     return dbConnection;
+//   },
+// };
+
+
 const { MongoClient } = require("mongodb");
-var dbConnection;
+require("dotenv").config();
+
+let dbConnection;
 
 module.exports = {
-  // connect to the mongodb database
   connectToDb: (cb) => {
     MongoClient.connect(process.env.URI)
       .then((client) => {
         dbConnection = client.db("sgdms");
         console.log("Successfully connected to mongo db server");
-        // function to fire after database connection
-        return cb();
+        if (typeof cb === "function") {
+          cb();
+        }
       })
       .catch((err) => {
         console.warn(`Error connecting to the database: ${err}`);
+        if (typeof cb === "function") {
+          cb(err);
+        }
       });
   },
 
-  // fetch and retrieve data from the mongo db
-  getDB: () => {
-    // return the database connection
-    return dbConnection;
-  },
+  getDB: () => dbConnection,
 };
