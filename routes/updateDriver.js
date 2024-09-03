01bc3423971706e6acb8bcc2264499d731020a93
Zeliang-Codetech <Zeliangcodetech@gmail.com>
2024-09-03 19:59:23 +0530

    update driver works in real time

--- routes/updateDriver.js
+++ routes/updateDriver.js
@@ -6,11 +6,7 @@ const { get } = require("http");
 var db;
 var drivers = [];
 var updatedDriver = [];
-router.get("/", (req, res) => {
-  res
-    .status(200)
-    .json({ message: "Update driver GET endpoint is working fine!" });
-});
+var bins = [];
 
 router.post("/", (req, res) => {
   const { driverId, name, number } = req.body;
@@ -66,38 +62,27 @@ router.post("/", (req, res) => {
   });
 });
 
-//   connectToDb((err) => {
-//     if (!err) {
-//       console.log("Connected to db from the updateDriver endpoint");
-//       db = getDB();
-//       db.collection("drivers")
-//         .find()
-//         .forEach((driver) => {
-//           // check if the driverId matches the id in the driver
-//           drivers.push(driver);
-//           if (driverId == driver._id) {
-//             var nameAndNum = { $set: { driverName: name, Phone: number } };
-//             db.collection("drivers").updateOne(
-//               {id: driverId},
-//               nameAndNum,
-//               function (err, res) {
-//                 if (err) {
-//                   throw err;
-//                 } else {
-//                   console.log("1 document updated!");
-//                   db.close();
-//                 }
-//               }
-//             );
-//             // update the driver name and phone no here.
-//           }
-//         })
-//         .then(() => {
-//           console.log(drivers);
-//         });
-//     }
-//   });
+router.get('/', (req, res) => {
+   connectToDb((err) => {
+     if (err) {
+       console.error("Database connection error:", err);
+       return res.status(500).json({ message: "Database connection error" });
+     }
 
-// });
+     const db = getDB();
+     (async () => {
+       try {
+         updatedDriver = await db.collection("drivers").find().toArray();
+         bins = await db.collection('bins').find().toArray();
+         res.status(200).json({
+           bins,  updatedDriver
+         });
+       } catch (err) {
+         console.log("Error ", err);
+       }
+     })();
+   });
+})
+
 
 module.exports = router;
